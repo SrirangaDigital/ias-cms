ba23cedf8adc85a01851ffcb9910ac0ece01f442
suresh07 <soori_rozee@yahoo.com>
2018-10-22 19:01:47 +0530

    Links in archive updated

--- application/views/flat/3-Listing/Meetings.php
+++ application/views/flat/3-Listing/Meetings.php
@@ -9,5 +9,7 @@
 	<?=$this->printBreadcrumb($path)?>
 	<ul>
 		<li><a href="<?=BASE_URL?>meeting/listing/AM2017">Annual meetings of the Indian Academy of Sciences - 2017</li>
+		<li><a href="<?=BASE_URL?>meeting/listing/MM2018">Mid Year meeting of the Indian Academy of Sciences - 2018</li>
+		<li><a href="<?=BASE_URL?>meeting/listing/AM2018">Annual meetings of the Indian Academy of Sciences - 2018</li>
 	</ul>
 </div>
